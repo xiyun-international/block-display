cd425c01e023674c364179d4f8c77b7eb8e75645
zhaoliang <kybetter@163.com>
2019-10-10 15:45:07 +0800

    feat: 加上扫描逻辑

---
+++ generate-data.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+const glob = require('glob');
+const write = require('write');
+
+glob('./blocks/packages/*/package.json', {}, (err, files) => {
+  if (!err) {
+    const result = [];
+    files.map(filePath => {
+      const pkgJson = require(filePath);
+      result.push({
+        name: pkgJson.name,
+        title: pkgJson.blockConfig.title,
+        categories: pkgJson.blockConfig.categories,
+        screenshot: pkgJson.blockConfig.screenshot,
+        version: pkgJson.version,
+        repository: pkgJson.repository.url,
+      })
+    })
+    write.sync('./src/blocks-data/data.json', JSON.stringify(result), {
+      overwrite: true,
+    })
+  }
+})
